701dd4b7518781f2d90b17bfaff8c0740f9396fd
Ben White <benjiman.white@gmail.com>
2019-03-10 12:36:40 -0700

    Add index response to list all contacts

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1> Hello World! </h1>'
+contacts = [
+    {
+        'id': 1,
+        'first_name': 'Margaret',
+        'last_name': 'Hamilton',
+        'phone_number': '6175557834',
+        'address': '17 Inman St. Cambridge, MA 02139, USA',
+        'email': 'margaret@hamiltontechnologies.com'
+    },
+    {
+        'id': 2,
+        'first_name': 'Ada',
+        'last_name': 'Lovelace',
+        'phone_number': '4803409032',
+        'address': 'Great Russell St, Bloomsbury, London, WC1B 3DG, UK',
+        'email': 'ada@computingrocks.net'
+    }
+]
+
+# Return all contacts
+@app.route('/contacts', methods=['GET'])
+def get_contacts():
+    return jsonify({'contacts': contacts})
