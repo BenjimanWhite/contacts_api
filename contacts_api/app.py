00d962c16b083f681be75898ee2a12097cdcac1a
Ben White <benjiman.white@gmail.com>
2019-03-12 00:00:27 -0700

    Add update functionality to app.

--- contacts_api/app.py
+++ contacts_api/app.py
@@ -40,9 +40,10 @@ def get_contact(id):
 @app.route('/contacts', methods=['POST'])
 def create_contact():
     
-    # if not request.get_json():
-    #     error_message = {'error': 'Bad request. You must supply data in json format.'}
-    #     return make_response(jsonify(error_message), 400)
+    # TODO TEST THIS
+    if not request.get_json():
+        error_message = {'error': 'Bad request. You must supply json data.'}
+        return make_response(jsonify(error_message), 400)
 
     if not contacts:
         new_contact = {
@@ -53,58 +54,154 @@ def create_contact():
             'id': contacts[-1]['id'] + 1
         }
 
-    required_field = 'first_name'
-    if not required_field in request.json:
+    if not 'first_name' in request.json:
         error_message = {'error': 'Bad request. You must supply at least a first name.'}
         return make_response(jsonify(error_message), 400)
 
-    if not request.json[required_field].strip():
+    #TODO TEST THIS
+    if not isinstance(request.json['first_name'], str):
+        error_message = {'error': 'Bad request. First name must be a string.'}
+        return make_response(jsonify(error_message), 400)
+
+    if not request.json['first_name'].strip():
         error_message = {'error': 'Bad request. You supplied an empty first name.'}
         return make_response(jsonify(error_message), 400)
 
-    new_contact['first_name'] = request.json['first_name']
+    new_contact['first_name'] = request.json['first_name'].strip()
     
     if 'last_name' in request.json:
-        new_contact['last_name'] = request.json['last_name']
+        if not isinstance(request.json['last_name'], str):
+            error_message = {'error': 'Bad request. Last name must be a string.'}
+            return make_response(jsonify(error_message), 400)
+
+        new_contact['last_name'] = request.json['last_name'].strip()
     else:
         new_contact['last_name'] = ''
 
     if 'phone_number' in request.json:
         number = request.json['phone_number']
         alphabetical_chars = '[a-zA-Z]+'
-        if isinstance(number, str) and re.search(alphabetical_chars, number):
-            error_message = {'error': 'Bad request. The phone number you provided contains letters.'}
-            return make_response(jsonify(error_message), 400)
+        if isinstance(number, str):
+            if re.search(alphabetical_chars, number):
+                error_message = {'error': 'Bad request. The phone number you provided contains letters.'}
+                return make_response(jsonify(error_message), 400)
 
-        new_contact['phone_number'] = request.json['phone_number']
+            new_contact['phone_number'] = request.json['phone_number'].strip()
+        else:
+            if not isinstance(number, int):
+                error_message = {'error': 'Bad request. The phone number must be an integer if it is not a string.'}
+                return make_response(jsonify(error_message), 400)
+
+            new_contact['phone_number'] = request.json['phone_number']
     else:
         new_contact['phone_number'] = ''
 
     if 'address' in request.json:
-        new_contact['address'] = request.json['address']
+        if not isinstance(request.json['address'], str):
+            error_message = {'error': 'Bad request. Address must be a string.'}
+            return make_response(jsonify(error_message), 400)
+
+        new_contact['address'] = request.json['address'].strip()
     else:
         new_contact['address'] = ''
 
     if 'email' in request.json:
+        if not isinstance(request.json['email'], str):
+            error_message = {'error': 'Bad request. Email must be a string.'}
+            return make_response(jsonify(error_message), 400)
+
         email_regex = re.compile(r'[\w\.-]+@[\w\.-]+')
         if not email_regex.search(request.json['email']):
             error_message = {'error': 'Bad request. Invalid email address.'}
             return make_response(jsonify(error_message), 400)
 
-        new_contact['email'] = request.json['email']
+        new_contact['email'] = request.json['email'].strip()
     else:
         new_contact['email'] = ''
 
     contacts.append(new_contact)
     return jsonify(new_contact), 201
 
+@app.route('/contacts/<int:id>', methods=['PUT'])
+def update_contact(id):
+    contact = list(filter(lambda contact: contact['id'] == id, contacts))
+
+    if len(contact) == 0:
+        error_message = {'error': 'Contact not found.'}
+        return make_response(jsonify(error_message), 404)
+
+    if not request.get_json():
+        error_message = {'error': 'Bad request. You must supply json data.'}
+        return make_response(jsonify(error_message), 400)
+
+    if 'first_name' in request.json:
+        if not isinstance(request.json['first_name'], str):
+            error_message = {'error': 'Bad request. First name must be a string.'}
+            return make_response(jsonify(error_message), 400)
+        
+        if not request.json['first_name'].strip():
+            error_message = {'error': 'Bad request. You supplied an empty first name.'}
+            return make_response(jsonify(error_message), 400)
+
+        contact[0]['first_name'] = request.json['first_name'].strip()
+
+    if 'last_name' in request.json:
+        if not isinstance(request.json['last_name'], str):
+            error_message = {'error': 'Bad request. Last name must be a string.'}
+            return make_response(jsonify(error_message), 400)
+
+        contact[0]['last_name'] = request.json['last_name'].strip()
 
 
+    if 'phone_number' in request.json:
+        if isinstance(request.json['phone_number'], str):
+            alphabetical_chars = '[a-zA-Z]+'
+            if re.search(alphabetical_chars, request.json['phone_number']):
+                error_message = {'error': 'Bad request. The phone number you provided contains letters.'}
+                return make_response(jsonify(error_message), 400)
+
+            contact[0]['phone_number'] = request.json['phone_number'].strip()
+        else:
+            if not isinstance(request.json['phone_number'], int):
+                error_message = {'error': 'Bad request. The phone number must be an integer if it is not a string.'}
+                return make_response(jsonify(error_message), 400)
+
+            contact[0]['phone_number'] = request.json['phone_number']
+
+    if 'address' in request.json:
+        if not isinstance(request.json['address'], str):
+            error_message = {'error': 'Bad request. Address must be a string.'}
+            return make_response(jsonify(error_message), 400)
+
+        contact[0]['address'] = request.json['address'].strip()
 
 
+    if 'email' in request.json:
+        if not isinstance(request.json['email'], str):
+            error_message = {'error': 'Bad request. Email must be a string.'}
+            return make_response(jsonify(error_message), 400)
 
+        email_regex = re.compile(r'[\w\.-]+@[\w\.-]+')
+        if not email_regex.search(request.json['email']):
+            error_message = {'error': 'Bad request. Invalid email address.'}
+            return make_response(jsonify(error_message), 400)
 
+        contact[0]['email'] = request.json['email'].strip()
 
+    return jsonify(contact[0]), 200
+
+## TODO
+# Add test case for non-string passed into create function (added today)
+# Add delete function
+# Test update, delete actions
+# Add HTTP Basic auth - store reference to the authenticated user in the modified record!
+# Test http basic auth
+# Add filter functionality by query string
+# Test filter Functionality by query string
+# document api
+# decompose app
+# Test distribution on another machine
+# send app
 
 
 
